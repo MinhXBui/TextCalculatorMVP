2a5815bebc011cec1dd9b0c22133796049ee1696
Insights & Report <67469703+MinhXBui@users.noreply.github.com>
2025-07-23 10:24:41 -0400

    Add files via upload

--- llm_api_async.py
+++ llm_api_async.py
@@ -24,7 +24,23 @@ async def multiple_llm_api_request (batch_request_list, model_configs):
     api_result_list.append(result_text_content)
   return api_result_list
 
+async def multiple_llm_api_request_with_limit (batch_request_list, model_configs, api_limiter):
+
+  llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite-preview-06-17", **model_configs)
+
+  async def process_single_request(request):
+      await api_limiter.wait()
+      individual_result = await llm.ainvoke(request)
+      return individual_result.content
+      
+  # The for loop append fail because we need a data holder for co-routine to run through not just a data list.
+  task_request_list = [process_single_request(request) for request in batch_request_list]
+  api_result_list = await(asyncio.gather(*task_request_list))
+  return api_result_list
+
 system_prompt = """You are an expert RAG agent focusing on Q&A with a question with provided context in the format of Question: (question asked), Context (context provided).
 Focus on response to the question do not add any commentary. If the context provided is not relevant simply return: No relevant context found"""
 
 model_configs = {"temperature": 0.0, "max_output_tokens": 120, "thinking_budget": 0}
+
+
